73555af4566855383046c2d9db36e3df4fd1b380
arifshaikha649 <arifshaikha649@gmail.com>
2023-10-11 16:53:58 +0530

    11 Oct 2023 - 1st Commit

--- contact.js
+++ contact.js
@@ -2,41 +2,50 @@
 
 
 function sendEmail() {
-    let name=document.getElementById('name').value;
-    let emailID=document.getElementById('email').value;
-    let mobile=document.getElementById('mobileNo').value;
-    let message=document.getElementById('message').value;
-    let Msgbody= `<br/><br/>Name: <b>${name}</b> <br/><br/> Email: <b>${emailID} </b><br/><br/> 
+    let name = document.getElementById('name').value;
+    let emailID = document.getElementById('email').value;
+    let mobile = document.getElementById('mobileNo').value;
+    let message = document.getElementById('message').value;
+    let Msgbody = `<br/><br/>Name: <b>${name}</b> <br/><br/> Email: <b>${emailID} </b><br/><br/> 
     Contact No.: <b>${mobile}</b><br/> <br/> Message: <b>${message}</b>`;
 
+    if (mobile.length == 10) {
 
+        Email.send({
+            SecureToken: "b41e2658-423f-42f3-acb4-6955a6e1b6b3",
+            To: 'ambiencedecorsales@gmail.com',
+            From: "ambiencedecorsales@gmail.com",
+            Subject: "New Enquiry ( " + new Date(). toLocaleDateString("en-US") + " )",
+            Body: Msgbody
+        }).then(
+            message => {
+                if (message == 'OK') {
+                    Swal.fire(
+                        'Thank You!',
+                        'We will get back to you at the earliest!',
+                        'success'
+                    )
+                }
+                else {
+                    Swal.fire(
+                        'Something went wrong!',
+                        ' ',
+                        'error'
+                    )
+                }
 
-    Email.send({
-        SecureToken: "68e3e409-b1e5-49e4-b994-452158987463",
-       
-        To: 'marketingtrick21@gmail.com',
-        From: "marketingtrick21@gmail.com",
-        Subject: "New Enquiry",
-        Body: Msgbody
-    }).then(
-        message => {
-            if(message=='OK')
-            {
-                Swal.fire(
-                    'Thank You!',
-                    'We will get back to you at the earliest!',
-                    'success'
-                  )
             }
-            else{
-                Swal.fire(
-                    'Something went wrong!',
-                    ' ',
-                    'error'
-                  )
-            }
+        );
+    }
 
-        }
-    );
+    else{
+        Swal.fire(
+            'Please input a valid!',
+            '10 digit mobile number',
+            'error'
+        )
+    }
 }
+
+
 
